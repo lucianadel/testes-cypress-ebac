4a3578c12a85780a4c32a13e9922cf5989a2c708
Luciana <luciana342010@hotmail.com>
2025-10-18 19:47:49 -0300

    subindo alteracao  clica botao editar

--- cypress/e2e/1-getting-started/agenda-contatos/agenda.cy.js
+++ cypress/e2e/1-getting-started/agenda-contatos/agenda.cy.js
@@ -1,9 +1,12 @@
- describe('Agenda de Contatos', () => {
+ describe('Agenda de Contatos - Testes de Inclusão, Alteração e Remoção', () => {
+
   beforeEach(() => {
+    // Abre o site antes de cada teste
     cy.visit('https://ebac-agenda-contatos-tan.vercel.app/')
   })
 
-  it('Deve adicionar um novo contato', () => {
+  // 1 TESTE DE INCLUSÃO
+  it.only('Deve adicionar um novo contato', () => {
     const nome = `Contato Teste ${Date.now()}`
     const email = `teste${Date.now()}@ebac.com.br`
     const telefone = '11988887777'
@@ -16,9 +19,43 @@
     // Clica no botão "Adicionar"
     cy.get('button.adicionar').click()
 
-    // Verifica se o novo contato aparece na tela
+    // Verifica se o contato foi adicionado na tela
     cy.contains(nome).should('be.visible')
     cy.contains(email).should('be.visible')
     cy.contains(telefone).should('be.visible')
-  })
-})
+    // cy.pause() // pausa aqui
+  }) 
+
+
+  // 1 TESTE DE alteracao
+  it.only('Deve editar um contato', () => {
+    const nome = `Contato Teste ${Date.now()}`
+    const email = `teste${Date.now()}@ebac.com.br`
+    const telefone = '11988887777'
+  
+    // Preenche os campos
+    cy.get('input[placeholder="Nome"]').type(nome)
+    cy.get('input[placeholder="E-mail"]').type(email)
+    cy.get('input[placeholder="Telefone"]').type(telefone)
+   
+    // Clica no botão "editar"
+    cy.get('.edit').last().click();
+    cy.get('.alterar').click();
+    cy.pause() 
+   
+      
+    // Verifica se o contato foi editado na  tela
+    cy.contains(nome).should('be.visible')
+    cy.contains(email).should('be.visible')
+    cy.contains(telefone).should('be.visible')
+    // cy.pause() // pausa aqui
+  }) 
+
+  
+});
+
+
+
+
+  
+
